3349aa00537b226cb88f394f29a3e0429718db92
C.Vignesh <162482102+VIGNESH1280@users.noreply.github.com>
2024-09-16 10:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	s.trim();
+	const spIdx=s.indexOf(' ');
+	if(spIdx=-1){
+		return s;
+	}
+	else{
+		return s.substrings(0, spIdx);
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
